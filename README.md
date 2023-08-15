e5ebcbfb169b3359d38d069f251eea8aef3cd221
FJrodafo <cnry.fjra@gmail.com>
2023-08-15 21:35:58 +0100

    v2

---
+++ README.md
@@ -0,0 +1,4 @@
+<picture>
+    <source media="(prefers-color-scheme: dark)" srcset="https://raw.githubusercontent.com/FJrodafo/Dotfiles/main/Assets/Dark.png">
+    <img alt="Logo" src="https://raw.githubusercontent.com/FJrodafo/Dotfiles/main/Assets/Light.png">
+</picture>
---
+++ Scripts/Install_node_and_npm.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# Download de LTS .tar.xz file at https://nodejs.org/en and put the file in your Downloads folder
+# Add the following command to the .bashrc file: export PATH=$PATH:/home/fjrodafo/nodejs/bin
+
+if node --version && npm --version; then
+    clear && neofetch
+    echo "You currently have Nodejs installed!"
+else
+    clear && cd ~/Downloads
+    # Check the version!
+    tar -xf node-v18.16.1-linux-x64.tar.xz
+    # Check the version!
+    mv node-v18.16.1-linux-x64 ~/nodejs
+    clear && neofetch
+    echo "Nodejs has been successfully installed!"
+fi
