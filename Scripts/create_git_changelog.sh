aaff97813e2de50977a23d702d98bdc6c28430c1
FJrodafo <cnry.fjra@gmail.com>
2024-04-21 18:21:29 +0100

    v49

---
+++ Scripts/create_git_changelog.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Check if we are in a Git repository
+if [ ! -d .git ]; then
+    echo "You are not in the root of a Git repository. Cannot generate CHANGELOG.md"
+    exit 1
+fi
+
+# Get current directory path
+DIR=$(pwd)
+
+# File name
+CHANGELOG="$DIR/CHANGELOG.md"
+
+# Save YAML header to file
+echo '```yaml' > "$CHANGELOG"
+
+# Run git log and save the output to file
+git log --graph --pretty=format:'Commit:  %h%nText:    %s%nAuthor:  %an%nDate:    %cD%n%n' --decorate >> "$CHANGELOG"
+
+# Add YAML closure to the end of the file
+echo '```' >> "$CHANGELOG"
+
+# Show confirmation message
+echo "Changelog generado en $CHANGELOG"
