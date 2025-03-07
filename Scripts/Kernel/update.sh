ecd785d29a4be7a5f23d89a829cd586b19e31967
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2025-03-07 18:27:57 +0000

    Fix update.sh

--- Scripts/Kernel/update.sh
+++ Scripts/Kernel/update.sh
@@ -3,5 +3,5 @@
 sudo update-initramfs -u
 sudo apt install linux-headers-$(uname -r)
 ls /usr/src
-echo -e 'Run ---> \033[1msudo\033[0m ./Downloads/Drivers/NVIDIA-Linux-x86_64-<version>.run'
+echo -e 'Run ---> \033[1msudo\033[0m ./Downloads/NVIDIA-Linux-x86_64-<version>.run'
 echo -e 'Then --> \033[1msystemctl\033[0m reboot'
