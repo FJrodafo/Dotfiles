02267171d1c5b6506b2d5ee169351ccf7096ad7f
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2025-03-07 12:30:30 +0000

    Update RiceInstaller.sh Information

--- Configs/Linux/Debian/RiceInstaller.sh
+++ Configs/Linux/Debian/RiceInstaller.sh
@@ -4,6 +4,12 @@
 #  ╠╦╝││  ├┤   ║│││└─┐ │ ├─┤│  │  ├┤ ├┬┘
 #  ╩╚═┴└─┘└─┘  ╩┘└┘└─┘ ┴ ┴ ┴┴─┘┴─┘└─┘┴└─
 
-#   Author: FJrodafo
-#   Website: https://fjrodafo.vercel.app
+# Author: Francisco José Rodríguez Afonso
+# E-mail: FJrodafo@gmail.com
+# GitHub: https://github.com/FJrodafo
+# Website: https://fjrodafo.vercel.app
 
+echo Author: Francisco José Rodríguez Afonso
+echo E-mail: FJrodafo@gmail.com
+echo GitHub: https://github.com/FJrodafo
+echo Website: https://fjrodafo.vercel.app
