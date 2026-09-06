c3698fb3f69863e92229561d3530d532740e4ac6
Francisco José Rodríguez Afonso <FJrodafo@gmail.com>
2026-09-06 19:41:57 +0100

    Update backup.sh [Debian/Trixie/PC]

--- Debian/Trixie/PC/home/fjrodafo/Scripts/backup.sh
+++ Debian/Trixie/PC/home/fjrodafo/Scripts/backup.sh
@@ -36,34 +36,34 @@ if [[ ! -e "$HARD_DRIVE" ]]; then
 fi
 
 cd "$HOME_DIR"
-# gitconfig
+# .gitconfig
 zip -P "$ZIP_PASSWORD" -r gitconfig.zip .gitconfig
 echo -e "${YELLOW}Saving gitconfig.zip${RESET}"
 mv gitconfig.zip "$HARD_DRIVE"
-# npmrc
+# .npmrc
 zip -P "$ZIP_PASSWORD" -r npmrc.zip .npmrc
 echo -e "${YELLOW}Saving npmrc.zip${RESET}"
 mv npmrc.zip "$HARD_DRIVE"
-# gem
+# .gem
 zip -P "$ZIP_PASSWORD" -r gem.zip .gem/credentials
 echo -e "${YELLOW}Saving gem.zip${RESET}"
 mv gem.zip "$HARD_DRIVE"
-# gnupg
+# .gnupg
 zip -P "$ZIP_PASSWORD" -r gnupg.zip .gnupgbackup
 echo -e "${YELLOW}Saving gnupg.zip${RESET}"
 mv gnupg.zip "$HARD_DRIVE"
-# m2
+# .m2
 zip -P "$ZIP_PASSWORD" -r m2.zip .m2/settings.xml
 echo -e "${YELLOW}Saving m2.zip${RESET}"
 mv m2.zip "$HARD_DRIVE"
-# ssh
+# .scripts
+zip -P "$ZIP_PASSWORD" -r scripts.zip .scripts
+echo -e "${YELLOW}Saving scripts.zip${RESET}"
+mv scripts.zip "$HARD_DRIVE"
+# .ssh
 zip -P "$ZIP_PASSWORD" -r ssh.zip .ssh
 echo -e "${YELLOW}Saving ssh.zip${RESET}"
 mv ssh.zip "$HARD_DRIVE"
-# Disks
-zip -P "$ZIP_PASSWORD" -r Disks.zip Disks
-echo -e "${YELLOW}Saving Disks.zip${RESET}"
-mv Disks.zip "$HARD_DRIVE"
 # Documents
 zip -P "$ZIP_PASSWORD" -r Documents.zip Documents
 echo -e "${YELLOW}Saving Documents.zip${RESET}"
@@ -72,10 +72,6 @@ mv Documents.zip "$HARD_DRIVE"
 zip -P "$ZIP_PASSWORD" -r Downloads.zip Downloads
 echo -e "${YELLOW}Saving Downloads.zip${RESET}"
 mv Downloads.zip "$HARD_DRIVE"
-# Favorites
-zip -P "$ZIP_PASSWORD" -r Favorites.zip Favorites
-echo -e "${YELLOW}Saving Favorites.zip${RESET}"
-mv Favorites.zip "$HARD_DRIVE"
 # Music
 zip -P "$ZIP_PASSWORD" -r Music.zip Music
 echo -e "${YELLOW}Saving Music.zip${RESET}"
@@ -88,18 +84,10 @@ mv Pictures.zip "$HARD_DRIVE"
 zip -P "$ZIP_PASSWORD" -r Public.zip Public
 echo -e "${YELLOW}Saving Public.zip${RESET}"
 mv Public.zip "$HARD_DRIVE"
-# Scripts
-zip -P "$ZIP_PASSWORD" -r Scripts.zip Scripts
-echo -e "${YELLOW}Saving Scripts.zip${RESET}"
-mv Scripts.zip "$HARD_DRIVE"
 # Templates
 zip -P "$ZIP_PASSWORD" -r Templates.zip Templates
 echo -e "${YELLOW}Saving Templates.zip${RESET}"
 mv Templates.zip "$HARD_DRIVE"
-# Temporary
-zip -P "$ZIP_PASSWORD" -r Temporary.zip Temporary
-echo -e "${YELLOW}Saving Temporary.zip${RESET}"
-mv Temporary.zip "$HARD_DRIVE"
 # Videos
 zip -P "$ZIP_PASSWORD" -r Videos.zip Videos
 echo -e "${YELLOW}Saving Videos.zip${RESET}"
